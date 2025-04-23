7b56db7279b65805d9887dcf453d648676909c75
flutter2-moon <saumil@moontechnolabs.com>
2025-04-23 17:39:17 +0530

    Update video_editor_controls_widget.dart

--- lib/shared/widgets/video/video_editor_controls_widget.dart
+++ lib/shared/widgets/video/video_editor_controls_widget.dart
@@ -31,7 +31,7 @@ class VideoEditorControlsWidget extends StatelessWidget {
               verticalDirection:
                   alignTop ? VerticalDirection.down : VerticalDirection.up,
               children: [
-                SizedBox(height: 30,),
+                const SizedBox(height: 30,),
                 const VideoEditorTrimBar(),
                 Padding(
                   padding: player.contentPadding,
